a284a212b266f25d49befc872a545e4242b7063a
Charlton Roberts <charltonroberts@gmail.com>
2013-10-12 19:34:19 -0400

    Creating new question, user

--- content-server/lib/db.js
+++ content-server/lib/db.js
@@ -1,4 +1,4 @@
-var APP_ID, Kaiseki, REST_API_KEY, getAnswers, kaiseki, settings;
+var APP_ID, Kaiseki, REST_API_KEY, createPatron, createQuestion, getAnswers, getPatron, kaiseki, newQuestion, question, settings;
 
 Kaiseki = require('kaiseki');
 
@@ -26,6 +26,94 @@ getAnswers = function(cb) {
   });
 };
 
-getAnswers(function(err, results) {
-  return console.log(results);
+getPatron = function(handle, cb) {
+  var params;
+  console.log('get patron ' + handle);
+  params = {
+    where: {
+      handle: handle
+    }
+  };
+  return kaiseki.getObjects('Patron', params, function(err, res, body, success) {
+    if (!success) {
+      return cb(err);
+    } else {
+      switch (body.length) {
+        case 1:
+          return cb(null, body[0]);
+        case 0:
+          return cb(null, false);
+        default:
+          return cb('Error: invalid number of users with handle ' + handle + '.');
+      }
+    }
+  });
+};
+
+createPatron = function(patron, cb) {
+  console.log('creating patron ' + patron.handle);
+  return kaiseki.createObject('Patron', patron, function(err, res, body, success) {
+    if (!success) {
+      return cb(err);
+    } else {
+      return cb(null, body.objectId);
+    }
+  });
+};
+
+newQuestion = function(patronId, body, cb) {
+  'new question';
+  var q;
+  q = {
+    patron: {
+      __type: 'Pointer',
+      className: 'Patron',
+      objectId: patronId
+    },
+    body: body
+  };
+  return kaiseki.createObject('Question', q, function(err, res, body, success) {
+    if (!success) {
+      return cb(err);
+    } else {
+      return cb(null, body.objectId);
+    }
+  });
+};
+
+createQuestion = function(question, cb) {
+  console.log('creating question');
+  return getPatron(question.patron.handle, function(err, result) {
+    if (err != null) {
+      return cb(err);
+    } else if (!result) {
+      console.log('No patron exists with that handle');
+      return createPatron(question.patron, function(err, result) {
+        if (err != null) {
+          return cb(err);
+        } else {
+          return newQuestion(result, question.body, cb);
+        }
+      });
+    } else {
+      console.log('we already have a patron with that handle');
+      return newQuestion(result, question.body, cb);
+    }
+  });
+};
+
+question = {
+  patron: {
+    first: 'Roseanne',
+    handle: '@rb27332491',
+    last: 'Barr'
+  },
+  body: 'Why do some stores in Tulsa have a "Sorry, we\'re open" sign?'
+};
+
+createQuestion(question, function(err, result) {
+  console.log(err);
+  return console.log(result);
 });
+
+exports.getAnswers = getAnswers;
--- content-server/src/db.coffee
+++ content-server/src/db.coffee
@@ -14,5 +14,61 @@ getAnswers = (cb)->
         unless success then cb(err)
         else cb(null, body)
 
-getAnswers (err, results)->
-    console.log results
+getPatron = (handle, cb)->
+    console.log 'get patron '+handle
+    params = 
+        where:
+            handle: handle
+
+    kaiseki.getObjects 'Patron', params, (err, res, body, success)->
+        unless success then cb(err)
+        else 
+            switch body.length
+                when 1 then cb(null, body[0])
+                when 0 then cb(null, false)
+                else cb('Error: invalid number of users with handle '+handle+'.')
+
+createPatron = (patron, cb)->
+    console.log 'creating patron '+patron.handle 
+    kaiseki.createObject 'Patron', patron, (err, res, body, success)->
+        unless success then cb(err)
+        else cb(null, body.objectId)
+
+newQuestion = (patronId, body, cb)->
+    'new question'
+    q = 
+        patron: 
+            __type: 'Pointer'
+            className: 'Patron'
+            objectId: patronId
+        body: body
+    kaiseki.createObject 'Question', q, (err, res, body, success)->
+        unless success then cb(err)
+        else
+            cb(null, body.objectId)
+
+createQuestion = (question, cb)->
+    console.log 'creating question'
+    getPatron question.patron.handle, (err, result)->
+        if err? then cb(err)
+        else if not result
+            console.log 'No patron exists with that handle'
+            createPatron question.patron, (err, result)->
+                if err? then cb(err)
+                else newQuestion result, question.body, cb
+                    
+        else
+            console.log 'we already have a patron with that handle' 
+            newQuestion result, question.body, cb
+
+question = 
+    patron: 
+        first: 'Roseanne'
+        handle: '@rb27332491'
+        last: 'Barr'
+    body: 'Why do some stores in Tulsa have a "Sorry, we\'re open" sign?'
+createQuestion question, (err, result)->
+    console.log err
+    console.log result
+
+exports.getAnswers = getAnswers
